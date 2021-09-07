477d0594751f288e8270386dbf037c2dc8894049
Ricardo Farias <ricardofariasxyz@gmail.com>
2021-09-07 00:16:26 -0300

    start commit

---
+++ src/main/java/rycardofarias/github/Animes_SpringBoot_API/AnimesSpringBootApiApplication.java
@@ -0,0 +1,15 @@
+package rycardofarias.github.Animes_SpringBoot_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimesSpringBootApiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AnimesSpringBootApiApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/rycardofarias/github/Animes_SpringBoot_API/AnimesSpringBootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package rycardofarias.github.Animes_SpringBoot_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimesSpringBootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

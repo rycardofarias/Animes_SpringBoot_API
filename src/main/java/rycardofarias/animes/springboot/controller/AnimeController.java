ba1cd2b72243118d518f6fdec4115a0757b17403
Ricardo Farias <ricardofariasxyz@gmail.com>
2021-09-07 01:38:54 -0300

    list animes GetMapping

--- src/main/java/rycardofarias/animes/springboot/controller/AnimeController.java
+++ src/main/java/rycardofarias/animes/springboot/controller/AnimeController.java
@@ -1,18 +1,27 @@
 package rycardofarias.animes.springboot.controller;
 
-import rycardofarias.animes.springboot.domain.Anime;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import rycardofarias.animes.springboot.domain.Anime;
+import rycardofarias.animes.springboot.util.DateUtil;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("anime")
+@RequestMapping("animes")
+@Log4j2
+@RequiredArgsConstructor
 public class AnimeController {
 
-    @GetMapping(path = "list")
+    private final DateUtil dateUtil;
+
+    @GetMapping
     public List<Anime> list(){
-        return List.of(new Anime("DBZ"), new Anime("CDZ"));
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return List.of(new Anime("DBZ"), new Anime("Naruto"));
     }
 }

fa13115b50c30f36f2c25b932c6a4369c01dab5a
Ricardo Farias <ricardofariasxyz@gmail.com>
2021-09-07 00:26:40 -0300

    create package start and domain class Anime

---
+++ src/main/java/domain/Anime.java
@@ -0,0 +1,20 @@
+package domain;
+
+public class Anime {
+    private String name;
+
+    public Anime() {
+    }
+
+    public Anime(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/start/ApplicationStart.java
+++ src/main/java/start/ApplicationStart.java
@@ -1,14 +1,14 @@
-package rycardofarias.github.Animes_SpringBoot_API;
+package start;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AnimesSpringBootApiApplication {
+public class ApplicationStart {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(AnimesSpringBootApiApplication.class, args);
+		SpringApplication.run(ApplicationStart.class, args);
 
 	}
 

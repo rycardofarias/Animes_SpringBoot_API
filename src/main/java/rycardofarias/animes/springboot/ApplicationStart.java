3bd181101a3ccd099e604089d7283959825c5407
Ricardo Farias <ricardofariasxyz@gmail.com>
2021-09-07 00:56:02 -0300

    move ApplicationStart

--- src/main/java/rycardofarias/animes/springboot/ApplicationStart.java
+++ src/main/java/rycardofarias/animes/springboot/ApplicationStart.java
@@ -1,11 +1,11 @@
-package rycardofarias.animes.springboot.start;
+package rycardofarias.animes.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "rycardofarias.animes.springboot")
+@ComponentScan
 public class ApplicationStart {
 
 	public static void main(String[] args) {

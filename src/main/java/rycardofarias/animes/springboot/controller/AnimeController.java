3f50934a1bcb477162489ea2546e0d577951fc46
Ricardo Farias <ricardofariasxyz@gmail.com>
2021-09-11 17:57:42 -0300

    list animes and findById

--- src/main/java/rycardofarias/animes/springboot/controller/AnimeController.java
+++ src/main/java/rycardofarias/animes/springboot/controller/AnimeController.java
@@ -2,10 +2,13 @@ package rycardofarias.animes.springboot.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import rycardofarias.animes.springboot.domain.Anime;
+import rycardofarias.animes.springboot.service.AnimeService;
 import rycardofarias.animes.springboot.util.DateUtil;
 
 import java.time.LocalDateTime;
@@ -18,10 +21,16 @@ import java.util.List;
 public class AnimeController {
 
     private final DateUtil dateUtil;
+    private final AnimeService animeService;
 
     @GetMapping
-    public List<Anime> list(){
+    public ResponseEntity<List<Anime>> list(){
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return List.of(new Anime("DBZ"), new Anime("Naruto"));
+        return ResponseEntity.ok(animeService.listAll());
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable long id){
+        return ResponseEntity.ok(animeService.listAll().get(0));
     }
 }

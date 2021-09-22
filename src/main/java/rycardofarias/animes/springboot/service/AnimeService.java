a01041f42c475730977f6357f59511a44794e675
Ricardo Farias <ricardofariasxyz@gmail.com>
2021-09-21 22:53:32 -0300

    save anime

--- src/main/java/rycardofarias/animes/springboot/service/AnimeService.java
+++ src/main/java/rycardofarias/animes/springboot/service/AnimeService.java
@@ -5,12 +5,18 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import rycardofarias.animes.springboot.domain.Anime;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
 
-    private List<Anime> animes = List.of(new Anime(1L,"DBZ"), new Anime(2L,"Naruto"));
+    private static List<Anime> animes;
+
+    static {
+        animes = new ArrayList<>(List.of(new Anime(1L,"DBZ"), new Anime(2L,"Naruto")));
+    }
 
     public List<Anime> listAll(){
          return animes;
@@ -21,5 +27,11 @@ public class AnimeService {
                 .filter(anime -> anime.getId().equals(id))
                 .findFirst()
             .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not Found"));
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3, 1000000));
+        animes.add(anime);
+        return anime;
     }
 }

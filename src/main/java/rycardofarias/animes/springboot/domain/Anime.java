7da95e012543bc1755d12e4dd073799650c4edd8
Ricardo Farias <ricardofariasxyz@gmail.com>
2021-09-24 23:55:03 -0300

    entity Anime

--- src/main/java/rycardofarias/animes/springboot/domain/Anime.java
+++ src/main/java/rycardofarias/animes/springboot/domain/Anime.java
@@ -3,10 +3,17 @@ package rycardofarias.animes.springboot.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 @Data
 @AllArgsConstructor
+@Entity
 public class Anime {
 
+    @Id
+    @GeneratedValue
     private Long id;
     private String name;
 }

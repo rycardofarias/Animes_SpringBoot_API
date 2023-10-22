6e76cc64c9d79d4b25325f09928eca9b29d67c63
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-10-22 17:58:59 -0300

    Pagination

--- src/main/java/rycardofarias/animes/springboot/service/AnimeService.java
+++ src/main/java/rycardofarias/animes/springboot/service/AnimeService.java
@@ -1,6 +1,8 @@
 package rycardofarias.animes.springboot.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import rycardofarias.animes.springboot.domain.Anime;
@@ -18,8 +20,8 @@ public class AnimeService {
 
     private final AnimeRepository animeRepository;
 
-    public List<Anime> listAll(){
-         return animeRepository.findAll();
+    public Page<Anime> listAll(Pageable pageable){
+         return animeRepository.findAll(pageable);
     }
 
     public List<Anime> findByName(String name){
